05a11e7b28e280b1c8a3488ce9a7f36d8a91876d
Andrew Dryga <andrew@dryga.com>
2016-04-03 13:40:44 +0300

    More examples

---
+++ views/user.js
@@ -0,0 +1,10 @@
+module.exports = {};
+
+// Maybe place inline docs here?
+module.exports.toApiView = function(user) {
+  return {
+    id: _id, //@ User ID
+    name, //@ User name
+    avatar //@ User avatar
+  } = user;
+}
